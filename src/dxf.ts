4a89f9f9d1506c828ef5d75fdb2b9d6bcc01389d
Alex <oruno999@gmail.com>
2024-02-21 18:20:30 +0700

    fix

---
+++ src/dxf.ts
@@ -0,0 +1,23 @@
+const DxfParser = require('dxf-parser');
+import fs from "fs/promises"
+// import Flatten from '@flatten-js/core'
+// const {Point, Vector, Circle, Line, Ray, Segment, Arc, Box, Polygon, Matrix, PlanarSet} = Flatten;
+
+// const parser = new DxfParser();
+
+console.log(123221)
+
+
+// export class DxfSquare{
+//   constructor(contentFile: any) {
+//     const data = parser.parseSync(contentFile);
+//   }
+//
+//   public async get(){
+//
+//   }
+// }
+//
+// const content = fs.readFile('samples\\t1.dxf', { encoding: 'utf8' });
+//
+// const dxfSquare = new DxfSquare(content)
